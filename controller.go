7353991f4f0895e1c95f5143bf2753f305cea152
Rosco Pecoltran <username@users.noreply.github.com>
2017-09-15 19:04:25 +0200

    remove glide.lock

--- controller.go
+++ controller.go
@@ -2,19 +2,19 @@ package feedify
 
 import (
 	"github.com/astaxie/beego"
-	"github.com/roscopecoltran/feedify/context"
+	"github.com/roscopecoltran/feedify/contextor"
 )
 
 type Controller struct {
 	beego.Controller
 }
 
-func (c *Controller) GetInput() *context.Input {
-	return &context.Input{c.Ctx.Input}
+func (c *Controller) GetInput() *contextor.Input {
+	return &contextor.Input{c.Ctx.Input}
 }
 
-func (c *Controller) GetCtx() *context.Context {
-	return &context.Context{c.Ctx}
+func (c *Controller) GetCtx() *contextor.Context {
+	return &contextor.Context{c.Ctx}
 }
 
 func (c *Controller) GetJsonData() interface{} {
